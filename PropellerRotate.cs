d065c76ea5910fa2012a16508a7f4fcb09c9185e
dingostarbuck <85776022+dingostarbuck@users.noreply.github.com>
2021-08-06 06:53:56 -0600

    Create PropellerRotate.cs

---
+++ PropellerRotate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotate : MonoBehaviour
+{
+
+    private float propRotate = 1216.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (Vector3.forward * propRotate * Time.deltaTime);
+    }
+}
+
+
+
+
+// challenge dunked ezpz 
